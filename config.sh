650e527bcd2d8904eb3096e8c4100ec12048d1bd
yelrose <270018958@qq.com>
2021-05-12 13:10:49 +0800

    update 2.1.4

--- config.sh
+++ config.sh
@@ -1,6 +1,7 @@
 # Define custom utilities
 # Test for macOS with [ -n "$IS_OSX" ]
 
+
 function pre_build {
     # Any stuff that you need to do before you start building the wheels
     # Runs in the root directory of this repository.
