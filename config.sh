8cce5ee08986c654784f1dd4f2cb6367262f0c8a
yelrose <270018958@qq.com>
2021-12-14 11:31:22 +0800

    remove distributed in __init__.py

--- config.sh
+++ config.sh
@@ -16,19 +16,18 @@ function run_tests {
     #fi
 
     #python -m pip install paddlepaddle
-    #python -m pip install paddlepaddle==2.1.0 -i https://mirror.baidu.com/pypi/simple
+    python -m pip install paddlepaddle==2.2.1 -i https://mirror.baidu.com/pypi/simple
     #==2.0.0-rc1 
     #python --version
-    #echo 'import sys
-#try:
-#    import pgl
-#    print("Import Success")
-#    sys.exit(0)
-#except Exception as e:
-#    print("Import Failed")
-#    print(e)
-#    sys.exit(1)' > test_import.py
-#    python test_import.py
-    :
+    echo 'import sys
+try:
+    import pgl
+    print("Import Success")
+    sys.exit(0)
+except Exception as e:
+    print("Import Failed")
+    print(e)
+    sys.exit(1)' > test_import.py
+    python test_import.py
 }
 
